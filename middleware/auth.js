90ebe0ec91f409e2c40ed588600b067e474bfbf2
Orphelio <orphelio.bourebia@gmail.com>
2022-10-05 17:41:57 +0200

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+       console.log(req.auth);
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
+
+/* const jwt = require("jsonwebtoken");
+
+// middleware auth
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "Invalid user ID";
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
+*/
+
